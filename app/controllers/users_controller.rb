0f70e10b5178d882805a647b387d33482d63416e
EC2 Default User <pinkpc45@gmail.com>
2019-12-31 11:58:33 +0000

    Top画面デザインのの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
   
+  
+  def index
+    @users = User.all
+  end
+  
+  #def index
+   # @users = User.paginate(page: params[:page])
+  #end
+ 
+  
   def show
     @user = User.find(params[:id])
 
